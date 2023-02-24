5a52b1f0e3aedb55c0f0f867703b6a9bab84e8d6
Enriclop11 <informenric@gmail.com>
2023-02-24 19:40:06 +0100

    Proyecto base

---
+++ uMind/src/main/java/com/uMind/uMind/UMindApplication.java
@@ -0,0 +1,13 @@
+package com.uMind.uMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UMindApplication.class, args);
+	}
+
+}
---
+++ uMind/src/test/java/com/uMind/uMind/UMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uMind.uMind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
