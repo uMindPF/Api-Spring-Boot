90ac4524c481700f393254f35dd621cc00594460
Enriclop11 <informenric@gmail.com>
2023-03-27 18:47:01 +0200

    Consultas flitradas pacientes

--- src/main/java/com/uMind/uMind/controller/PacienteConsultasController.java
+++ src/main/java/com/uMind/uMind/controller/PacienteConsultasController.java
@@ -5,6 +5,7 @@ import com.uMind.uMind.servicio.PacienteService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,15 @@ public class PacienteConsultasController {
     @GetMapping("/consultas/pacientes")
     public List<Paciente> getPacientes() {
         return pacienteService.getPacientes();
+    }
+
+    @GetMapping("/consultas/pacientes/{id}")
+    public Paciente getPacienteById(@PathVariable Integer id) {
+        return pacienteService.getPacienteById(id);
+    }
+
+    @GetMapping("/consultas/pacientes/nombre/{nombre}")
+    public List<Paciente> getPacienteByNombre(@PathVariable String nombre) {
+        return pacienteService.getPacienteByNombre(nombre);
     }
 }
